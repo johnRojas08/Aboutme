ab728c56e3e28c1f521b31277b3c25a8d621d85b
johnRojas08 <johnrojas0808@gmail.com>
2024-12-11 21:32:46 +0100

    Añadiendo código a Perfil

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 
 public class Main {
     public static void main(String[] args) {
+        Perfil perfil = new perfil(
+          "John Rojasa",
+          "Soy un curioso por naturaleza.",
+          Arrays.asList("Caminar", "Leer", "Viajar"),
+          Arrays.asList("Barbacoa", "Arroz", "Helados"),
+          Arrays.asList("Soy Economista", "Tengo un máster en MK", "Toco la guitarra")
+
+        );
 
         }
     }
--- src/Perfil.java
+++ src/Perfil.java
@@ -3,17 +3,32 @@ import java.util.List;
 public class Perfil {
     private String nombre;
     private String historia;
+    private List<String> pasatiempos;
+    private List<String> comidasFavoritas;
+    private List<String> datosCuriosos;
 
-    public Perfil(String nombre, String historia) {
+    public Perfil(String nombre, String historia, List<String> pasatiempos, List<String> comidasFavoritas, List<String> datosCuriosos) {
         this.nombre = nombre;
         this.historia = historia;
-}
-    public String getNombre() {
-        return nombre;
+        this.pasatiempos = pasatiempos;
+        this.comidasFavoritas = comidasFavoritas;
+        this.datosCuriosos = datosCuriosos;
     }
 
-    public String getHistoria() {
-        return historia;
+    public String getFavoritos() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Pasatiempos:\n");
+        pasatiempos.forEach(p -> sb.append("- ").append(p).append("\n"));
+
+        sb.append("\nComidas Favoritas:\n");
+        comidasFavoritas.forEach(c -> sb.append("- ").append(c).append("\n"));
+
+        return sb.toString();
+    }
+
+    public String getDatoCurioso() {
+        Random random = new Random();
+        return datosCuriosos.get(random.nextInt(datosCuriosos.size()));
     }
 }
 
